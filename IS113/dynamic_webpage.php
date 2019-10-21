2b602cb888453a0ed3f09bbd265887a5e09d1ada
one-ice <53385835+one-ice@users.noreply.github.com>
2019-10-21 22:32:00 +0800

    Create dynamic_webpage.php

---
+++ IS113/dynamic_webpage.php
@@ -0,0 +1,16 @@
+<?php
+// 1.constant variable
+// must be uppercase, case sensitive
+define("CONATANT1","php");
+echo CONSTANT1;
+// 2.null coalscing
+// behave like a if condition: if $b is not null, $b is assigened to $a. Otherwise $c is assigned to $a
+$a = $b ?? $c
+// 3. inicialize an array:
+$arr = array()
+$arr = array("London","Washington","the new york")
+$arr[] = "Shang Hai"
+// 4.get the size of array for forloop
+count($arr) 
+// 5.
+?>
